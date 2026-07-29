2bfba7fddcf9d780bc555c527c0209922bf37e96
Patel Jainil <pateljainil8849@gmail.com>
2026-07-29 22:21:57 +0530

    Added Auto Invoice Number

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-function login() {
+function login(event){
+
+    event.preventDefault();
 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username === "admin" && password === "1234") {
+    let button = document.querySelector("button");
 
-        window.location.href = "dashboard.html";
+    if(username==="admin" && password==="admin"){
 
-    } else {
+        button.innerHTML = "Logging in...";
+        button.disabled = true;
 
-        alert("Invalid Username or Password!");
+        setTimeout(function(){
+
+            alert("Login Successful ✅");
+
+            window.location.href = "dashboard.html";
+
+        },1000);
+
+    }else{
+
+        alert("❌ Invalid Username or Password");
 
     }
 
